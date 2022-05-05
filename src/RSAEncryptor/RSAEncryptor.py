22805363ed05a8efa0b457050f8750a9138cc80f
Maciej Fender <fendermaciej@gmail.com>
2022-05-06 00:24:18 +0200

    usuniecie syfu

--- src/RSAEncryptor/RSAEncryptor.py
+++ src/RSAEncryptor/RSAEncryptor.py
@@ -32,12 +32,3 @@ class RSAEncryptor:
     def decrypt_str(self, value: list) -> str:
 
         return self.decrypt(value).decode()
-
-
-
-def int_to_bytes(x: int) -> bytes:
-    return x.to_bytes((x.bit_length() + 7) // 8, 'big')
-
-
-def int_from_bytes(xbytes: bytes) -> int:
-    return int.from_bytes(xbytes, 'big')
